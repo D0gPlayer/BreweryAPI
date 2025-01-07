52cfff06b0da61298db74226b97ef896da4a87ef
Dog Player <evgeniy.belov@softline.com>
2025-01-07 16:14:32 +0200

    Added repository pattern

---
+++ BreweryAPI/Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BreweryAPI.Data
+{
+    public interface IRepository<T> where T : class
+    {
+
+        public DbSet<T> DbSet { get; set; }
+        public Task<bool> Add(T entity);
+        public Task<bool> Update(Guid Id);
+        public Task<bool> Delete(Guid Id);
+    }
+    
+}
---
+++ BreweryAPI/Data/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq.Expressions;
+
+namespace BreweryAPI.Data
+{
+     public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DataContext _DbContext;
+        public Repository(DataContext dataContext) 
+        {
+            _DbContext = dataContext;
+            DbSet = dataContext.Set<T>();
+        }
+
+        public DbSet<T> DbSet { get; set; }
+
+        public async Task<bool> Add(T entity)
+        {
+            await _DbContext.AddAsync(entity);
+            var saved = await _DbContext.SaveChangesAsync();
+            return saved > 0;
+        }
+
+        public async Task<bool> Delete(Guid Id)
+        {
+            var entity = DbSet.Find(Id);
+            if(entity is null) return false;
+
+            _DbContext.Remove(entity);
+            var deleted = await _DbContext.SaveChangesAsync();
+            return deleted > 0;
+        }
+
+        public async Task<bool> Update(Guid Id)
+        {
+            var entity = DbSet.Find(Id);
+            if(entity is null) return false;
+
+            _DbContext.Update(entity);
+            var updated = await _DbContext.SaveChangesAsync();
+            return updated > 0;
+
+        }
+    }
+}
